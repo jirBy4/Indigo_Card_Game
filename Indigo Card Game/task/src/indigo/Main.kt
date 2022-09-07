4ca839cb44ad8c1f07830d64ef5907e49dc19e98
jirBy4 <109446944+jirBy4@users.noreply.github.com>
2022-09-06 18:02:14 -0700

    Update Main.kt

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -35,6 +35,7 @@ class Game {
     private var playerScore = 0; private var botScore = 0; private var tablePoints = 0
 
     fun playerOrder(): String {
+        ///decides who plays first
         println("Play first? ")
         choice = readln().lowercase()
         return when (choice) {
@@ -44,6 +45,7 @@ class Game {
     }
 
     fun openingMsg() {
+        /// adds all the cards into the deck, adds cards to the bot and player's hands respectfully and puts the initial cards on the table
         unusedCards += pointCards + lameCards; unusedCards.shuffle()
         botHand = handRefresh() as MutableList<String>; playerHand = handRefresh() as MutableList<String>
         tableCards = unusedCards.take(4) as MutableList<String>; unusedCards.removeAll(unusedCards.take(4))
@@ -52,13 +54,17 @@ class Game {
         println("Initial cards on the table: ${tableCards.joinToString(" ")}\n") }
 
     fun tableReader() = if (tableCards.size == 0 ) println("No cards on the table") else println("${tableCards.size} cards on the table, and the top card is ${tableCards[tableCards.lastIndex]}")
-
+    ///reads how many cards are on the table
+    
     private fun tableReset() {
+        ///resets table
         topCard = " "; tablePoints = 0; tableCards.clear() }
 
     private fun handRefresh() = unusedCards.take(6).also { unusedCards.removeAll(it) }
+    /// refreshes hand of player or bot
 
     fun playerMove() {
+        /// checks how many cards are in the player's hand and whether a hand refesh is needed or not. If not, moves on to player's card choice
         if (playerHand.size == 0 && unusedCards.size == 0) return
         if(playerHand.size == 0 && unusedCards.size >= 6) playerHand = handRefresh() as MutableList<String>
         println("Cards in hand: ")
@@ -68,6 +74,7 @@ class Game {
     }
 
     private fun moveChoice() {
+        /// plays the chosen card based off of player input
         println("\nChoose a card to play (1-${playerHand.size}): ")
         choice = readln()
         if (choice == "exit") {
@@ -77,16 +84,18 @@ class Game {
             choice.toIntOrNull() == null -> moveChoice()
             choice.toInt() !in 1..playerHand.size -> moveChoice()
             else -> {
+                /// puts the input choice onto the field and off the player's hand. Then checks whether the card played wins all the cards on the table
                 tableCards += playerHand[choice.toInt() - 1]; winCheck(playerHand[choice.toInt() - 1], "Player")
                 playerHand.removeAt(choice.toInt() - 1) }
         }
     }
 
     private fun suitCheck (hand: MutableList<String>) = if (!hand.any { it.last() == topCard.last() }) "empty" else hand.filter { it.last() == topCard.last() }.random()
-
+    /// checks if bot has any cards of the suit of the top card
     private fun numCheck (hand: MutableList<String>) = if (!hand.any{ it[0] == topCard[0] }) "empty" else hand.filter { it[0] == topCard[0] }.random()
-
+    /// checks if bot has any cards of the number of the top card
     fun botMove() {
+        /// checks how many cards are in the bot's hand and whether a hand refesh is needed or not. If not, moves on to bot's card choice
         if (botHand.size == 0 && unusedCards.size == 0) return
         if (botHand.size == 0 && unusedCards.size >= 6) botHand = handRefresh() as MutableList<String>
         println(botHand.joinToString(" "))
@@ -94,6 +103,7 @@ class Game {
         tableCards += cardPlayed; winCheck(cardPlayed, "Computer"); botHand.removeAt(botHand.indexOf(cardPlayed)) }
 
     private fun botCheck(): String = when {
+        /// plays the only card in the hand if there's 1 card, otherwise moves to the bot's input choice
         botHand.size == 1 -> botHand[0]
         tableCards.size == 0 -> botMoveChoice()
         suitCheck(botHand) != "empty" -> suitCheck(botHand)
@@ -107,19 +117,24 @@ class Game {
                 maxSuit = i; suitCount = hand.count { it.last() == i }
             }
         }
+        /// decides which suit the bot has the greatest amount of 
         for (j in mutableListOf('1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K')) {
             if (hand.count { it.first() == j } > numCount) {
                 maxNum = j; numCount = hand.count { it.first() == j }
             }
         }
+        /// decides which number the bot has the greatest amount of  
         val choice = when {
             suitCount >= numCount -> botHand.filter {it.last() == maxSuit }.random()
             numCount > 1 && suitCount < numCount -> botHand.filter {it[0] == maxNum }.random()
-            else -> botHand.random() }
+            else -> botHand.random() } 
+            /// plays card based off of max number of suits/numbers in hand. So if the bot has 3 clubs and 1 spade, it plays a random club 
+            /// or if the bot has 3 kings and 1 eight, it plays a random king.
         return choice
     }
 
     private fun winCheck(card: String, winner: String) {
+        /// checks if the card played is the winning move
         if (card in pointCards) tablePoints++
 
         if (topCard != " " && card[card.lastIndex] == topCard[topCard.lastIndex] || card[0] == topCard[0]) {
@@ -129,6 +144,7 @@ class Game {
     }
 
     private fun scoreAdd(winner: String) {
+        /// adds score based on whether the bot or the player won
         if (winner == "Player") {
             playerScore += tablePoints; playerYard += tableCards; tableReset() }
         else {
@@ -138,6 +154,7 @@ class Game {
     private fun score() = println("Score: Player $playerScore - Computer $botScore\nCards: Player ${playerYard.size} - Computer ${botYard.size}")
 
     fun finalScore() {
+        /// reads the final score of the game
         tableReader()
         if (tableCards.size != 0 ) scoreAdd(lastWinner)
         tablePoints += 3
